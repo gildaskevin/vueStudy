b045f0a78b24f135fba69fe7c67d69237eb6ca6f
gildaskevin <gildaskevin6@gmail.com>
2019-04-12 13:57:28 +0200

    try1

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+new Vue({el: '#app', data: {message: "ma premiere vue", message2:"Nous sommes à la date"+new Date().toLocaleString()}});
+new Vue({el: "#v-control", data:{seen:true}});
+new Vue({el: '#forControl', data:{ todos: [ {text:"learn power of javascript"}, {text: "learn framexorks"}, {text:"learn Vue framexork"}, {text:"learn loopback nodeservice"}]}});
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
 new Vue({el: '#app', data: {message: "ma premiere vue", message2:"Nous sommes à la date"+new Date().toLocaleString()}});
+new Vue({
+	el: "#app-5", 
+	data:{
+		seen:true, 
+		message:"10", 
+		text:"mon texte à traiter pour Juliette"
+	}, 
+	methods:{
+		doSome:function () {
+			calcule(message, text)
+		}
+	}
+});
+new Vue({el: '#forControl', data:{ todos: [ {text:"learn power of javascript"}, {text: "learn framexorks"}, {text:"learn Vue framexork"}, {text:"learn loopback nodeservice"}]}});
+
+
+function calcule(arg, txt) {
+	arg+=arg;
+	ret="le nouveau message est: "+txt+" "+arg;
+	console.log(ret);
+	return ret;
+}
